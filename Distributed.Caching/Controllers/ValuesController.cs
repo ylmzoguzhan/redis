8eda03848daa73a604e074aa52f2bfd95c939a13
ylmzoguzhan <80750095+ylmzoguzhan@users.noreply.github.com>
2023-10-01 00:15:58 +0300

    In-Memory Caching ve Distributed Caching

---
+++ Distributed.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Distributed.Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly private IDistributedCache _distributedCache;
+
+        public ValuesController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        [HttpPost]
+        public void Set(string key, string value)
+        {
+            _distributedCache.SetString(key, value, options: new()
+            {
+                AbsoluteExpiration = DateTimeOffset.UtcNow.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+        }
+        [HttpGet]
+        public string Get(string key)
+        {
+            return _distributedCache.GetString(key);
+        }
+    }
+}
---
+++ InMemory.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly private IMemoryCache _memoryCache;
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+        //[HttpPost]
+        //public void Set(string name)
+        //{
+        //    _memoryCache.Set("role", name);
+        //}
+        //[HttpGet]
+        //public string Get()
+        //{
+        //    return _memoryCache.Get<string>("role");
+        //}
+        //[HttpPost("remove")]
+        //public void Remove(string key)
+        //{
+        //    _memoryCache.Remove(key);
+        //}
+        [HttpGet("set")]
+        public void SetDate()
+        {
+            _memoryCache.Set<DateTime>("date", DateTime.Now, options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+        }
+        [HttpGet("get")]
+        public DateTime GetDate()
+        {
+            return _memoryCache.Get<DateTime>("date");
+        }
+    }
+}

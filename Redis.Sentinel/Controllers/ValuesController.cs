273e6efc9b90e0832faf05a12b081cb2207866ee
ylmzoguzhan <80750095+ylmzoguzhan@users.noreply.github.com>
2023-10-01 22:53:24 +0300

    Redis Sentinel

---
+++ Redis.Sentinel/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Redis.Sentinel.Services;
+
+namespace Redis.Sentinel.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("set")]
+        public IActionResult SetValue(string key, string value)
+        {
+            var redis = RedisService.RedisMasterDB();
+            redis.StringSet(key, value);
+            return Ok();
+        }
+        [HttpGet("get")]
+        public IActionResult GetValue(string key)
+        {
+            var redis = RedisService.RedisMasterDB();
+            var data = redis.StringGet(key);
+            return Ok(data.ToString());
+        }
+    }
+}
---
+++ Redis.Sentinel/Services/RedisService.cs
@@ -0,0 +1,46 @@
+﻿using StackExchange.Redis;
+
+namespace Redis.Sentinel.Services
+{
+    public class RedisService
+    {
+        static ConfigurationOptions sentinelOptions => new()
+        {
+            EndPoints =
+            {
+                {"localhost",6383 },
+                {"localhost",6384 },
+                {"localhost",6385 }
+            },
+            CommandMap = CommandMap.Sentinel,
+            AbortOnConnectFail = false
+        };
+        static ConfigurationOptions masterOptions => new()
+        {
+            AbortOnConnectFail = false
+        };
+        static public IDatabase RedisMasterDB()
+        {
+            ConnectionMultiplexer sentinelConnection = ConnectionMultiplexer.SentinelConnect(sentinelOptions);
+            System.Net.EndPoint masterEndPoint = null;
+            foreach (var item in sentinelConnection.GetEndPoints())
+            {
+                var server = sentinelConnection.GetServer(item);
+                if (!server.IsConnected)
+                    continue;
+                masterEndPoint = server.SentinelGetMasterAddressByName("mymaster");
+                break;
+            }
+            var localMasterIP = masterEndPoint.ToString() switch
+            {
+                "172.18.0.2:6379" => "localhost:6379",
+                "172.18.0.3:6379" => "localhost:6380",
+                "172.18.0.4:6379" => "localhost:6381",
+                "172.18.0.5:6379" => "localhost:6382"
+            };
+            ConnectionMultiplexer masterConnection = ConnectionMultiplexer.Connect(localMasterIP);
+            var db = masterConnection.GetDatabase();
+            return db;
+        }
+    }
+}

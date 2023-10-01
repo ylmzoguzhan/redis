6bf7ed211dfa7ad2d48540a36768a07257b1be51
ylmzoguzhan <80750095+ylmzoguzhan@users.noreply.github.com>
2023-10-01 10:11:00 +0300

    Pub/Sub

---
+++ Redis.Pub.Sub.Example/Program.cs
@@ -0,0 +1,11 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost:2023");
+ISubscriber subscriber = redis.GetSubscriber();
+
+while (true)
+{
+    Console.Write("Mesaj :");
+    string message = Console.ReadLine();
+    subscriber.Publish("mychannel",message);
+}
---
+++ Redis.Pub.Sub.Subscriber.Example/Program.cs
@@ -0,0 +1,10 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost:2023");
+ISubscriber subscriber = redis.GetSubscriber();
+
+subscriber.Subscribe("mychannel", (channel, value) =>
+{
+    Console.WriteLine(value);
+});
+Console.Read();
